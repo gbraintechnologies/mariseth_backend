37ed8c5d3f1cf9b2726f904726f800fb9d277078
okwesi <okwesi73@gmail.com>
2025-06-04 15:07:25 +0000

    ♻️ Refactor farm serializer to handle livestock and crops fields

--- apps/farm/serializers/farm.py
+++ apps/farm/serializers/farm.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from apps.accounts.serializers.users import ShortUserSerializer
 from apps.farm.models import Farm, FarmProduct, Product
+from apps.farm.serializers.products import ShortProductSerializer
 from apps.farm.utils import generate_farm_id
 from apps.shared.models import District, Region
 from apps.shared.serializers.regions import DistrictSerializer, ShortRegionSerializer
@@ -14,18 +15,23 @@ class ShortFarmSerializer(serializers.ModelSerializer):
 
 
 class FarmProductSerializer(serializers.ModelSerializer):
-    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    product = ShortProductSerializer()
 
     class Meta:
         model = FarmProduct
-        fields = ('id', 'product', 'is_main_product', 'notes')
+        fields = ('id', 'product', 'is_main_product')
         read_only_fields = ('id',)
 
 
 class FarmSerializer(serializers.ModelSerializer):
-    products = FarmProductSerializer(many=True)
     region = serializers.PrimaryKeyRelatedField(queryset=Region.objects.all(), required=False)
     district = serializers.PrimaryKeyRelatedField(queryset=District.objects.all(), required=False)
+    livestock = serializers.ListField(
+        child=serializers.PrimaryKeyRelatedField(queryset=Product.objects.filter(is_active=True), required=False
+                                                 ))
+    crops = serializers.ListField(
+        child=serializers.PrimaryKeyRelatedField(queryset=Product.objects.filter(is_active=True), required=False
+                                                 ))
 
     class Meta:
         model = Farm
@@ -34,7 +40,7 @@ class FarmSerializer(serializers.ModelSerializer):
             'size', 'size_metric', 'livestock_kept', 'has_access_to_market',
             'irrigation', 'use_of_fertilizers', 'farming_methods', 'provide_training',
             'government_ngo_support', 'specify_support', 'areas_of_assistance',
-            'land_ownership', 'other_specification', 'products'
+            'land_ownership', 'other_specification', 'livestock', 'crops'
         )
         read_only_fields = ('id',)
 
@@ -53,41 +59,73 @@ class FarmSerializer(serializers.ModelSerializer):
         validated_data['created_by'] = request.user
         validated_data['organization'] = request.organization
         validated_data['farm_id'] = generate_farm_id(validated_data['name'])
-        farm_products_data = validated_data.pop('products')
+        livestock = validated_data.pop('livestock')
+        crops = validated_data.pop('crops')
         farm = Farm.objects.create(**validated_data)
-        for farm_product_data in farm_products_data:
-            FarmProduct.objects.create(farm=farm, **farm_product_data)
+        if livestock:
+            for livestock_data in livestock:
+                FarmProduct.objects.create(farm=farm, product=livestock_data, is_main_product=True)
+        if crops:
+            for crop_data in crops:
+                FarmProduct.objects.create(farm=farm, product=crop_data, is_main_product=True)
         return farm
 
     def update(self, instance, validated_data):
         # TODO: ADD SIGNALS TO LOG CHANGES
+        incoming_crop_products = validated_data.pop('crops', [])
+        incoming_livestock_products = validated_data.pop('livestock', [])
+        desired_products_for_farm = set(incoming_crop_products + incoming_livestock_products)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        instance.updated_by = self.context['request'].user
         instance.save()
+        current_farm_product_relations = instance.farmproduct_set.all()
+        for existing_relation in current_farm_product_relations:
+            if existing_relation.product not in desired_products_for_farm and existing_relation.is_active:
+                existing_relation.is_active = False
+                existing_relation.save()
+        for product_to_be_linked in desired_products_for_farm:
+            relation, created = instance.farmproduct_set.get_or_create(
+                product=product_to_be_linked,
+                defaults={'is_main_product': False, 'is_active': True}
+            )
+            if not created and not relation.is_active:
+                relation.is_active = True
+                relation.save()
         return instance
 
 
 class FullFarmSerializer(serializers.ModelSerializer):
     created_by = ShortUserSerializer()
-    products = serializers.SerializerMethodField()
+    crops = serializers.SerializerMethodField()
+    livestock = serializers.SerializerMethodField()
     region = ShortRegionSerializer()
     district = DistrictSerializer()
 
     class Meta:
         model = Farm
         fields = (
-            'id', 'farm_id', 'farm_type', 'type', 'name', 'location','region', 'district',
+            'id', 'farm_id', 'farm_type', 'type', 'name', 'location', 'region', 'district',
             'size', 'size_metric', 'livestock_kept', 'has_access_to_market',
             'irrigation', 'use_of_fertilizers', 'farming_methods', 'provide_training',
             'government_ngo_support', 'specify_support', 'areas_of_assistance',
             'land_ownership', 'other_specification', 'created_by', 'date_created',
-            'products'
+            'crops', 'livestock'
         )
         read_only_fields = ('id', 'created_by', 'updated_by', 'date_created')
 
-    def get_products(self, obj):
-        return FarmProductSerializer(obj.farmproduct_set.filter(is_active=True), many=True).data
+    def get_crops(self, obj):
+        crop_products = obj.farmproduct_set.filter(
+            is_active=True,
+            product__type='crop'
+        )
+        return FarmProductSerializer(crop_products, many=True).data
+
+    def get_livestock(self, obj):
+        livestock_products = obj.farmproduct_set.filter(
+            is_active=True,
+            product__type='livestock'
+        )
+        return FarmProductSerializer(livestock_products, many=True).data
 
 
 class FarmDeleteSerializer(serializers.ModelSerializer):
