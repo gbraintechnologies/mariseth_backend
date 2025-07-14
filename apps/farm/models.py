80d08467ece683c68cca0df752de548586ce84db
okwesi <okwesi73@gmail.com>
2025-07-14 21:15:41 +0000

    "Add color field to Product model and serializers

--- apps/farm/models.py
+++ apps/farm/models.py
@@ -155,6 +155,7 @@ class Product(BaseModel):
     season_end = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True, null=True)
     breed = models.CharField(max_length=100, blank=True, null=True)
+    color = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.id} - {self.name}"
