a50c4d3186c3c3c2d5ff9fbc405a19050ec96c54
okwesi <okwesi73@gmail.com>
2025-09-25 14:31:21 +0000

    fix unique constraint od ssnit number

--- apps/hr/serializers/employee.py
+++ apps/hr/serializers/employee.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from apps.accounts.serializers.users import ShortUserSerializer
 from apps.hr.models import Employee, EmployeeContract, EmployeeDisciplinaryAction, EmployeeEmergencyContact,     EmployeeQualification, LeaveRequest, LeaveType
