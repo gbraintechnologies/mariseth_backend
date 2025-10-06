1d975b6466b81303c8559bb300e4838e660e371e
devthec3 <devthec3@gmail.com>
2025-10-06 09:22:44 +0000

    Update production.py (#90)
    changed coolify backend url

--- mariseth/settings/production.py
+++ mariseth/settings/production.py
@@ -16,9 +16,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://*",
     "https://*",
-    "https://v48ws000c8wg0gcgg880sg4o.65.109.122.43.sslip.io",
-    "https://s4ckwk8g4cc88k8sgww084ko.65.109.122.43.sslip.io",
-    "https://z4k0ks808s84sw044s8kogww.65.109.122.43.sslip.io"
+    "https://backend.65.109.122.43.sslip.io"
 ]
 
 SIMPLE_JWT = {
